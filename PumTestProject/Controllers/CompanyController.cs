01256126669ca1520a99dc8a39aafcd63b6ce0dc
mskalacki <mskalacki@gmail.com>
2020-02-02 22:19:29 +0100

    Firs controller responding for call

---
+++ PumTestProject/Controllers/CompanyController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace PumTestProject.Controllers
+{
+    public class CompanyController : ApiController
+    {
+       [HttpGet]
+        public string Foo(string name)
+        {
+            return "Dogadało się!";
+        }
+    }
+}
